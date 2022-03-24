b912ab4e253a1828f57d9c22d07db4fd366961e3
Harsh Vyas <vyasharsh70@gmail.com>
2022-03-24 14:31:48 +0530

    first commit

---
+++ LinkedList/MakeLL.java
@@ -0,0 +1,49 @@
+
+class LinkedList {
+    Node head;
+    public void print() {
+        Node curr = head;
+        while(curr != null) {
+            System.out.print(curr.data + " -> ");
+            curr = curr.next;
+        }
+    }
+    public boolean isEmpty() {
+        return head == null;
+    }
+    public int size() {
+        Node curr = head;
+        int c = 0;
+        while(curr != null) {
+            c++;
+            curr = curr.next;
+        }
+        return c;
+    }
+    public void addFirst(int x) {
+        Node n = new Node(x);
+        if(isEmpty()) {
+            head = n;
+        } else {
+            n.next = head;
+            head = n;
+        }
+    }
+    private class Node {
+        int data;
+        Node next;
+        public Node(){}
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+}
+
+class MakeLL {
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+        ll.addFirst(4);ll.addFirst(3);ll.addFirst(5);
+        ll.print();
+    }
+}
